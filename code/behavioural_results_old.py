3f1003106520a9ffda1890f59964e2588c434d94
Kayla Rail <kayla.rail@students.mq.edu.au>
2026-06-01 16:23:01 +1000

    other changes

--- code/behavioural_results_old.py
+++ code/behavioural_results_old.py
@@ -323,30 +323,6 @@ d = d_cost[d_cost['block'] > 17] # equating number of train and test blocks for 
 dd = d.groupby(['subject_id', 'session_num', 'phase',
                            'probe_condition'])['acc'].mean().reset_index()
 
-d_multi = d.copy()
-
-fig, ax = plt.subplots(1, len(days_lab), squeeze=False, figsize=(8,6), sharey=True)
-for a, day in zip(ax.flat, days_lab):
-      sns.lineplot(
-          data=d_multi[d_multi['session_num'] == day],
-          x='block',
-          y='acc',
-          hue='probe_condition',
-          legend=False,
-          errorbar=None,
-          ax=a
-      )
-      a.set_title(f'Day {day}')
-      a.set_ylim(0.5, 1)
-      a.vlines(data=d_multi[d_multi['session_num'] == day],
-                 x=11,
-                 ymin=0.5,
-                 ymax=1,
-                 linestyle='--')
-plt.legend()
-plt.tight_layout()
-plt.show()
-
 dd_wide = (
   dd.pivot_table(
       index=['subject_id', 'session_num', 'probe_condition'],
